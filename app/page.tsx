c3b5b3a178c4dbb97acbc5e46726923c742a4ed7
Chainstrument <minitutoriel@gmail.com>
2024-09-17 15:59:19 +0200

    add button

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
-
-import Button from "./components/Button";
-
+ 
 export default function Home() {
   return ( 
     <div className="p-8 ">
       <main className="w-full h-screen flex justify-center items-center flex-col gap-4">
-        <div>Hello</div>
-        <div>Hello</div>
+        <div className="bg-zinc-100 shadow-lg p-8 border border-zinc-300 rounded-lg">
+            Hello
+        </div>
+         
+        
       </main>
       
     </div>
