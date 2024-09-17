46a3551583853861122a6ccc8dbec227d07fe483
Chainstrument <minitutoriel@gmail.com>
2024-09-17 15:54:21 +0200

    centered text

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
-import Image from "next/image";
+
 import Button from "./components/Button";
 
 export default function Home() {
   return ( 
     <div className="p-8 ">
-        <Button name="Hello" className="bg-green-500 text-white px-4 py-2 rounded-md"/>
-        <section className="flex flex-col items-center justify-center">
-         <span className="text-lg font-medium">Bonjour</span>
-        </section>
-
+      <main className="w-full h-screen flex justify-center items-center">
+        Hello
+      </main>
+      
     </div>
   );
 }
