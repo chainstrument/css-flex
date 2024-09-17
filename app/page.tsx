da0fca7e9e4c857d488c1f40c09b83e64a7b9bc1
Chainstrument <minitutoriel@gmail.com>
2024-09-17 15:23:14 +0200

    add test text

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return ( 
-    <div className="flex-container text-center">
-      test
+    <div className="p-8 ">
+        <button className="btn-primary bg-violet-200 p-5 rounded-xl">click me</button>
+        
+
+        <span className="text-lg font-medium">Bonjour</span>
+        
     </div>
   );
 }
