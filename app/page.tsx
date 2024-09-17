e49933f76efc7573e5a94fc98fd5eac1e517cdfa
Chainstrument <minitutoriel@gmail.com>
2024-09-17 17:29:43 +0200

    add shadow

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return ( 
     <div className="">
       <main className="flex flex-col items-center justify-center">
-        <div className=" border w-full h-screen  bg-zinc-50 max-w-sm shadow-lg rounded-lg">
+        <div className=" border w-full h-screen flex flex-col justify-start items-center bg-zinc-50 max-w-sm shadow-lg rounded-lg">
  
           <h1 className="font-bold text-xl">Hello</h1>
           <p className="text-sm/6 text-black/50">
@@ -13,7 +13,7 @@ export default function Home() {
             id tenetur voluptatum a minima eos? Dolorum cupiditate, accusantium excepturi fuga expedita consequatur esse amet error ipsa delectus.
           </p>
 
-          <button className="rounded-md bg-purple-400 px-4 py-2 text-white hover:bg-purple-500">
+          <button className="rounded-md bg-purple-600 shadow-lg px-4 py-2 text-white hover:bg-purple-500">
           click me
         </button>
         </div>
