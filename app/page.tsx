1bd5351c399fc7ce2d3558cf3fb274021abdb61e
Chainstrument <minitutoriel@gmail.com>
2024-09-17 15:56:06 +0200

    flex col

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,9 @@ import Button from "./components/Button";
 export default function Home() {
   return ( 
     <div className="p-8 ">
-      <main className="w-full h-screen flex justify-center items-center">
-        Hello
+      <main className="w-full h-screen flex justify-center items-center flex-col gap-4">
+        <div>Hello</div>
+        <div>Hello</div>
       </main>
       
     </div>
