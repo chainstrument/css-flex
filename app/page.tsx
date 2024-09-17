d3c30b4caa11a25b0513de9acefe56af51eb91c3
Chainstrument <minitutoriel@gmail.com>
2024-09-17 17:13:33 +0200

    example

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
+import Example from "./example1";
+
  
 export default function Home() {
   return ( 
     <div className="p-8 ">
       <main className="w-full h-screen flex justify-center items-center flex-col gap-4">
-        <div className="bg-zinc-100 shadow-lg p-8 border border-zinc-300 rounded-lg">
+        <div className="bg-blue-100 shadow-lg p-8 border border-blue-300 rounded-lg">
             Hello
         </div>
          
-        
+        <Example />
       </main>
       
     </div>
