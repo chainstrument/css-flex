bd429ef6740e3d7bd8645ee5985c32c824a0c259
Chainstrument <minitutoriel@gmail.com>
2024-09-17 17:25:08 +0200

    add opacity color and size

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,12 @@ export default function Home() {
     <div className="">
       <main className="flex flex-col items-center justify-center">
         <div className=" border w-full h-screen  bg-zinc-50 max-w-sm shadow-lg rounded-lg">
-          <h1 className="font-bold text-xs">Hello</h1>
-          <h1 className="font-bold text-sm">Hello</h1>
-          <h1 className="font-bold text-base">Hello</h1>
-          <h1 className="font-bold text-lg">Hello</h1>
+ 
           <h1 className="font-bold text-xl">Hello</h1>
-          <h1 className="font-bold text-2xl">Hello</h1>
-          <h1 className="font-bold text-3xl">Hello</h1>
+          <p className="text-sm/6 text-black/50">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque tempore impedit est 
+            id tenetur voluptatum a minima eos? Dolorum cupiditate, accusantium excepturi fuga expedita consequatur esse amet error ipsa delectus.
+          </p>
         </div>
 
 
